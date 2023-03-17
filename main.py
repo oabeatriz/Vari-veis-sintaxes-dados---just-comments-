ba0ecdf00c8c2ed7de2e5bfeca2933b19bce8f82
Ana Beatriz de Oliveira Assis <107081846+oabeatriz@users.noreply.github.com>
2023-03-17 14:45:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+# entradas, inputs e tipos de variáveis.
+
+################## #entradas no console.
+
+# nome = input("Digite o seu nome: ")
+# print("Olá" , nome)  ou  print(f"Olá, {nome}")
+
+# numero = input("Digite um número: ")
+# print(f"O número digitado foi: {numero} ")  ou  print("O número digitado foi: , numero")
+
+#num1 = int(input("Digite o primeiro número: "))
+#num2 = int(input("Digite o segundo número: ")) #o int soma os valores.
+#soma = num1 + num2 
+#print(f"A soma de {num1} e {num2} é igual a {soma} ")
+  
+# num1 = float(input("Digite um número: "))
+# num2 = float(input("Digite outro número: "))
+# soma = num1 + num2
+# print(f"A soma de {num1} e {num2} é igual a: {soma} ")
+
+##################### variaveis no console
+
+#inteiro = 10
+#float = 3.14
+#texto = "Olá, mundo."
+#verdadeiro = True
+#falso = False 
+#print(inteiro, type(inteiro))
+#print(float, type(float))
+#print(texto, type(texto))
+#print(verdadeiro, type(verdadeiro))
+#print(falso, type(falso))
+
+################################################ operadores aritméticos
+
+# a = 5
+# b = 6
+# soma = a + b
+# subtracao = a - b
+# multiplicacao = a * b 
+# divisao = a / b 
+# resto = a % b 
+# potencia = a ** b
+# raiz = a ** 0.5  
+# import math
+# raiz = math.sqrt(a)
+
+
+
+
+
+
+
+
+
+
+# obs: selecionar tudo em comentario > ctrl + ;
